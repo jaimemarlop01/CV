44dd24a381b854a76706039c018e01d6a045d3ea
Jaime Martínez López <jaimemarlop01@gmail.com>
2026-04-27 12:02:49 +0200

    Initial commit: split HTML into css/, js/, img/

---
+++ js/main.js
@@ -0,0 +1,4 @@
+  const obs = new IntersectionObserver((entries) => {
+    entries.forEach(e => { if (e.isIntersecting) { e.target.classList.add('revealed'); } });
+  }, { threshold: 0.1 });
+  document.querySelectorAll('.reveal').forEach(el => obs.observe(el));
